c02f8e22fb72ec6635444ba30ef4ebb3c9373e91
Ling Fung Chan <justinlfchan@gmail.com>
2023-02-25 07:21:56 +0000

    created Card FormField Loader components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,7 @@ const App = () => {
 
       {/* Page Body Content */}
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        {/* Create Routes (Frontend) */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="create-post" element={<CreatePost />} />
