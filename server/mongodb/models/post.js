e02a28e438a2c57ed8013f0255e21adafe55f521
Ling Fung Chan <justinlfchan@gmail.com>
2023-05-05 15:11:14 +0100

    Revert "updated post-model post-schema name"
    This reverts commit 54865d4a33d6d05b7a2def612df15036ea5de447.

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const PostSchema = new mongoose.Schema({
+const Post = new mongoose.Schema({
   name: { type: String, required: true },
   prompt: { type: String, required: true },
   photo: { type: String, required: true },
 });
 
-const Post = mongoose.model("Post", PostSchema);
+const PostSchema = mongoose.model("Post", Post);
 
-export default Post;
+export default PostSchema;
